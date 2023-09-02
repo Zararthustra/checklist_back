94e4a51fd1d4ca7a149a33ffd3bada681a0747d1
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2023-09-02 16:39:08 +0200

    Update models.py

--- src/checklist/models.py
+++ src/checklist/models.py
@@ -5,16 +5,18 @@ import uuid
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
+    )
     name = models.CharField(max_length=200)
-    color = models.CharField(max_length=20, null=True)
+    color = models.CharField(max_length=50, null=True)
     isHidden = models.BooleanField(null=True)
 
 
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
+    )
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
